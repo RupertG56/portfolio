d81fd755690b68b7ddfd5acc51d2b4049f6bc247
Ryan <rupertg56@gmail.com>
2026-07-11 15:44:29 -0600

    Fix BlogPostDocument from inadvertently hiding inherited member IsDeleted.

--- Portfolio.Api/BlogPost/BlogPostDocument.cs
+++ Portfolio.Api/BlogPost/BlogPostDocument.cs
@@ -10,7 +10,6 @@ public class BlogPostDocument : BaseDocument
     public string Summary { get; set; } = string.Empty;
     // The blog post content in Markdown format
     public string ContentMarkdown { get; set; } = string.Empty;
-    public bool IsDeleted { get; set; } = false;
     public int Revision { get; set; } = 1;
     public string? InternalNotes { get; set; } = null;
 }
